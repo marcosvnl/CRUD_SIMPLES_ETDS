c77d6e540126f0f1ad53f037922b2126d2ebb858
Marcos Vinicius <maarcosvnl@gmail.com>
2020-09-15 21:44:32 -0300

    Add project files.

---
+++ CRUD_SIMPLES_ETDS/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace CRUD_SIMPLES_ETDS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dgvDados = new System.Windows.Forms.DataGridView();
+            this.btnNovo = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnConsultra = new System.Windows.Forms.Button();
+            this.btnExibir = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(45, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(45, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "NOME";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(45, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "NÚMERO";
+            // 
+            // dgvDados
+            // 
+            this.dgvDados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDados.Location = new System.Drawing.Point(48, 237);
+            this.dgvDados.Name = "dgvDados";
+            this.dgvDados.Size = new System.Drawing.Size(403, 150);
+            this.dgvDados.TabIndex = 3;
+            // 
+            // btnNovo
+            // 
+            this.btnNovo.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnNovo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNovo.Location = new System.Drawing.Point(48, 186);
+            this.btnNovo.Name = "btnNovo";
+            this.btnNovo.Size = new System.Drawing.Size(75, 23);
+            this.btnNovo.TabIndex = 4;
+            this.btnNovo.Text = "Novo";
+            this.btnNovo.UseVisualStyleBackColor = false;
+            this.btnNovo.Click += new System.EventHandler(this.btnNovo_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnEditar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditar.Location = new System.Drawing.Point(129, 186);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(75, 23);
+            this.btnEditar.TabIndex = 5;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = false;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnConsultra
+            // 
+            this.btnConsultra.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnConsultra.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConsultra.Location = new System.Drawing.Point(210, 186);
+            this.btnConsultra.Name = "btnConsultra";
+            this.btnConsultra.Size = new System.Drawing.Size(75, 23);
+            this.btnConsultra.TabIndex = 6;
+            this.btnConsultra.Text = "Consultar";
+            this.btnConsultra.UseVisualStyleBackColor = false;
+            this.btnConsultra.Click += new System.EventHandler(this.btnConsultra_Click);
+            // 
+            // btnExibir
+            // 
+            this.btnExibir.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnExibir.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExibir.Location = new System.Drawing.Point(291, 186);
+            this.btnExibir.Name = "btnExibir";
+            this.btnExibir.Size = new System.Drawing.Size(75, 23);
+            this.btnExibir.TabIndex = 7;
+            this.btnExibir.Text = "Exibir";
+            this.btnExibir.UseVisualStyleBackColor = false;
+            this.btnExibir.Click += new System.EventHandler(this.btnExibir_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnExcluir.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExcluir.Location = new System.Drawing.Point(376, 186);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(75, 23);
+            this.btnExcluir.TabIndex = 8;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = false;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(106, 53);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(179, 20);
+            this.txtId.TabIndex = 9;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(106, 98);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(179, 20);
+            this.txtNome.TabIndex = 10;
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(106, 146);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(179, 20);
+            this.txtNumero.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(507, 439);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnExibir);
+            this.Controls.Add(this.btnConsultra);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnNovo);
+            this.Controls.Add(this.dgvDados);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Cadastro";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDados)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dgvDados;
+        private System.Windows.Forms.Button btnNovo;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnConsultra;
+        private System.Windows.Forms.Button btnExibir;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtNumero;
+    }
+}
+
---
+++ CRUD_SIMPLES_ETDS/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace CRUD_SIMPLES_ETDS
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conexao;
+        SqlCommand comandoSLQ;
+        SqlDataReader dr;
+        SqlDataAdapter da;
+        string strSQL;
+        // Priximo passo criar a estaciação da conexão com o banco de dados no evento do Botão "Novo".
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNovo_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conexao = new SqlConnection(); // podemos escrever a string de conexão dentro dos ().
+                conexao.ConnectionString = Properties.Settings.Default.CTS; // Busca a string de conexão app.config
+                strSQL = "INSERT INTO tb_usuario(nome, numero) VALUES (@nome, @numero)";
+                comandoSLQ = new SqlCommand(strSQL, conexao);
+                comandoSLQ.Parameters.AddWithValue("@nome", txtNome.Text);
+                comandoSLQ.Parameters.AddWithValue("@numero", txtNumero.Text);
+
+                conexao.Open();
+                comandoSLQ.ExecuteNonQuery(); // executar os comandos
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+                conexao = null;
+                comandoSLQ = null;
+            }
+        }
+
+        private void btnExibir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conexao = new SqlConnection(); // podemos escrever a string de conexão dentro dos ().
+                conexao.ConnectionString = Properties.Settings.Default.CTS; // Busca a string de conexão app.config
+                strSQL = "SELECT * FROM tb_usuario";
+                DataSet dados = new DataSet();
+                da = new SqlDataAdapter(strSQL, conexao);
+                conexao.Open();
+                da.Fill(dados);
+                dgvDados.DataSource = dados.Tables[0];
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+                conexao = null;
+            }
+        }
+
+        private void btnConsultra_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conexao = new SqlConnection(); // podemos escrever a string de conexão dentro dos ().
+                conexao.ConnectionString = Properties.Settings.Default.CTS; // Busca a string de conexão app.config
+                strSQL = "SELECT * FROM tb_usuario WHERE id = @id";
+                comandoSLQ = new SqlCommand(strSQL, conexao);
+                comandoSLQ.Parameters.AddWithValue("@id", txtId.Text);
+                conexao.Open();
+                dr = comandoSLQ.ExecuteReader();
+                while(dr.Read())
+                {
+                    txtNome.Text = Convert.ToString(dr["nome"]);
+                    txtNumero.Text = Convert.ToString(dr["numero"]);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+                conexao = null;
+                comandoSLQ = null;
+            }
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conexao = new SqlConnection(); // podemos escrever a string de conexão dentro dos ().
+                conexao.ConnectionString = Properties.Settings.Default.CTS; // Busca a string de conexão app.config
+                strSQL = "UPDATE tb_usuario SET nome = @nome, numero = @numero WHERE id = @id";
+                comandoSLQ = new SqlCommand(strSQL, conexao);
+                comandoSLQ.Parameters.AddWithValue("@id", txtId.Text);
+                comandoSLQ.Parameters.AddWithValue("@nome", txtNome.Text);
+                comandoSLQ.Parameters.AddWithValue("@numero", txtNumero.Text);
+                conexao.Open();
+                comandoSLQ.ExecuteNonQuery(); // executar os comandos
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+                conexao = null;
+                comandoSLQ = null;
+            }
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conexao = new SqlConnection(); // podemos escrever a string de conexão dentro dos ().
+                conexao.ConnectionString = Properties.Settings.Default.CTS; // Busca a string de conexão app.config
+                strSQL = "DELETE tb_usuario WHERE id = @id";
+                comandoSLQ = new SqlCommand(strSQL, conexao);
+                comandoSLQ.Parameters.AddWithValue("@ID", txtId.Text);
+                conexao.Open();
+                comandoSLQ.ExecuteNonQuery(); // executar os comandos
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+                conexao = null;
+                comandoSLQ = null;
+            }
+        }
+    }
+}
